8c3f6494ceeefd852e02fb5bd159fd4ef742617d
getBoolean <danielwnewville@gmail.com>
2022-10-31 03:07:15 -0500

    Use flutter in expectBuildClean

--- test/ensure_build_test.dart
+++ test/ensure_build_test.dart
@@ -3,6 +3,8 @@
 import 'package:build_verify/build_verify.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+const _kDefaultCommand = ['flutter', 'run', 'build_runner', 'build', '--delete-conflicting-outputs'];
+
 void main() {
-  test('ensure_build', expectBuildClean);
+  test('ensure_build', () => expectBuildClean(customCommand: _kDefaultCommand));
 }
