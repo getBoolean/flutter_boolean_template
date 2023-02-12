9e9fc9bca80fb0f3cad1f532cb678747685438bd
getBoolean <danielwnewville@gmail.com>
2023-02-12 02:50:50 -0600

    ignore workspace settings

--- packages/features/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ packages/features/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Boolean\fvm\default"
+export "FLUTTER_ROOT=C:\Users\Boolean\fvm\versions\3.7.3"
 export "FLUTTER_APPLICATION_PATH=B:\Documents\GitHub\flutter_boolean_template\packages\features"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
