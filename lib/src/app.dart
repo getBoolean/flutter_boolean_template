26f841502c00660930420e75c38059d8e66724dd
getBoolean <danielwnewville@gmail.com>
2023-02-11 19:01:24 -0600

    App uses flex_color_scheme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:convenient_test/convenient_test.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:models/models.dart';
 
@@ -16,9 +17,9 @@ class App extends StatelessWidget {
     return ConvenientTestWrapperWidget(
       child: MaterialApp(
         title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
+        theme: FlexThemeData.light(scheme: FlexScheme.mandyRed),
+        darkTheme: FlexThemeData.dark(scheme: FlexScheme.mandyRed),
+        themeMode: ThemeMode.system,
         navigatorKey: App.navigatorKey,
         home: MyHomePage(title: '${person.firstName} ${person.lastName}'),
       ),
