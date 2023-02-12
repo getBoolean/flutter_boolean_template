50b5568c96530bb83e8754df3420602c02555456
getBoolean <danielwnewville@gmail.com>
2023-02-11 20:42:55 -0600

    Fix main test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,11 +8,12 @@
 import 'package:app_workspace/src/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(const ProviderScope(child: App()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
