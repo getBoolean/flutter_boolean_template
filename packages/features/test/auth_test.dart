206e263dffd55affa078f414da29082bd7e1ce16
getBoolean <danielwnewville@gmail.com>
2023-02-11 20:19:32 -0600

    Add test scripts

---
+++ packages/features/test/auth_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(
+    'group description',
+    () {
+      test(
+        'test description',
+        () {
+          // Arrange
+          // Act
+          // Assert
+        },
+      );
+    },
+  );
+}
