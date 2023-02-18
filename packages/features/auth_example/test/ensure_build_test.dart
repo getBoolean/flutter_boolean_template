43f414f68b65ed54a84d17d2e38af68f6534c969
getBoolean <danielwnewville@gmail.com>
2023-02-18 00:25:14 -0600

    ensure build uses default command when possible

--- packages/features/auth_example/test/ensure_build_test.dart
+++ packages/features/auth_example/test/ensure_build_test.dart
@@ -3,8 +3,6 @@
 import 'package:build_verify/build_verify.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-const _kBuildCommand = ['dart', 'run', 'build_runner', 'build'];
-
 void main() {
-  test('ensure_build', () => expectBuildClean(customCommand: _kBuildCommand));
+  test('ensure_build', expectBuildClean);
 }
