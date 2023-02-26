89519b48a549ae7230271efb8e6f2fabe56ab2d1
getBoolean <getboolean@gmail.com>
2023-02-25 18:06:11 -0600

    format

--- packages/app/lib/app_method_channel.dart
+++ packages/app/lib/app_method_channel.dart
@@ -11,7 +11,8 @@ class MethodChannelApp extends AppPlatform {
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
   }
 }
