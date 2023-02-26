fbd6da4529bf128d595c17d99eab7aafa9476634
getBoolean <getboolean@gmail.com>
2023-02-26 00:57:42 -0600

    format

--- packages/utils/env/lib/src/env/debug_env.dart
+++ packages/utils/env/lib/src/env/debug_env.dart
@@ -12,7 +12,7 @@ class DebugEnv implements AppEnv, AppEnvFields {
   // Providing a default value for everything allows the app to be build
   // without setting up the .env file. This might be useful for someone
   // who wants to build the app without setting up cloud services.
-  
+
   @override
   @EnviedField(varName: 'KEY1', defaultValue: 'test1')
   final String key1 = _Env.key1;
