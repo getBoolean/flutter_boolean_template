49c686b2832d7d4f75360db70595fba1ea7e7009
getBoolean <getboolean@gmail.com>
2023-02-25 18:00:53 -0600

    app is converted to plugin

---
+++ packages/app/lib/app_method_channel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import 'app_platform_interface.dart';
+
+/// An implementation of [AppPlatform] that uses method channels.
+class MethodChannelApp extends AppPlatform {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = const MethodChannel('app');
+
+  @override
+  Future<String?> getPlatformVersion() async {
+    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    return version;
+  }
+}
