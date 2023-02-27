c244f23a30dbedc772dbbc1ca74e938536cc103a
getBoolean <getboolean@gmail.com>
2023-02-26 23:53:54 -0600

    Add log provider

--- packages/utils/logs/lib/logs.dart
+++ packages/utils/logs/lib/logs.dart
@@ -1,6 +1,12 @@
 library logs;
 
+import 'package:logging/logging.dart';
+import 'package:riverpod/riverpod.dart';
+
 export 'package:logging/logging.dart';
 
 export 'src/log_config.dart';
 export 'src/logger_widget.dart';
+
+// ignore: avoid_types_on_closure_parameters
+final logProvider = Provider.autoDispose.family((Ref ref, String name) => Logger(name));
