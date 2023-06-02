df3703d2b26f9df68d979e4d7a2d48823d55e787
getBoolean <getboolean@gmail.com>
2023-06-02 00:16:18 -0500

    Remove unused imports

--- packages/features/routing/lib/src/router_provider.dart
+++ packages/features/routing/lib/src/router_provider.dart
@@ -1,10 +1,7 @@
-import 'package:assets/assets.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logs/logs.dart';
 
 import 'router/app_router.dart';
-import 'ui/app_scaffold.dart';
 // Source: https://codewithandrea.com/articles/flutter-bottom-navigation-bar-nested-routes-gorouter-beamer/
 
 // Keep in mind that the navigation position of each page won't be preserved until StatefulShellRoute from
