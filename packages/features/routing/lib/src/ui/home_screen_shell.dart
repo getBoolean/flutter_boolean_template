f4e2ab3f19fc4c6d8a2c44be917efc814dad43ff
getBoolean <getboolean@gmail.com>
2023-06-02 00:16:30 -0500

    Add wrapped route widget

--- packages/features/routing/lib/src/ui/home_screen_shell.dart
+++ packages/features/routing/lib/src/ui/home_screen_shell.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:logs/logs.dart';
 
 @RoutePage()
-class HomeScreenShell extends StatelessWidget {
+class HomeScreenShell extends StatelessWidget implements AutoRouteWrapper {
   const HomeScreenShell({super.key});
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement HomeScreen widget to use instead of Placeholder
-    return const LoggerWidget(child: Placeholder());
+    return const Placeholder();
+  }
+
+  @override
+  Widget wrappedRoute(BuildContext context) {
+    return LoggerWidget(child: this);
   }
 }
