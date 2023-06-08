eba8a81269325f2c16a26c1215a9f2f5becb4870
getBoolean <getboolean@gmail.com>
2023-06-08 01:17:15 -0500

    Add domain model usage

--- packages/app/lib/app.dart
+++ packages/app/lib/app.dart
@@ -1,11 +1,12 @@
 library app;
 
 import 'package:auth_example/auth/auth.dart';
+import 'package:domain/domain.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:localization/localization.dart';
-import 'package:models/models.dart';
 import 'package:routing/routing.dart';
 
 /// The main app widget at the root of the widget tree.
@@ -14,8 +15,13 @@ class App extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final person = Person(firstName: 'John', lastName: 'Doe', age: Age(42));
-    debugPrint(person.toJson());
+    final user = UserAccount(
+      name: Name(firstName: 'John', lastName: 'Doe'),
+      email: Email('test@gmail.com'),
+      id: '5646532',
+    );
+    user.name.toOption().fold(() => null, (name) => debugPrint(name.toString()));
+    
     final String auth = ref.watch(authProvider);
     debugPrint(auth);
 
