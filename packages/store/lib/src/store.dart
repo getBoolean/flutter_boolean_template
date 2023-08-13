344cafa6bbdecc096c7c242f482b6d11ed4c107d
getBoolean <getboolean@gmail.com>
2023-08-13 01:46:32 -0500

    Add `FutureStore` and `StreamStore`

--- packages/store/lib/src/store.dart
+++ packages/store/lib/src/store.dart
@@ -1,7 +1,31 @@
+import 'package:stock/stock.dart';
+
+const store = Store();
+
 /// {@template store}
 /// Offline-first database and file storage
 /// {@endtemplate}
 class Store {
   /// {@macro store}
   const Store();
+
+  FutureStore get future => const FutureStore();
+  StreamStore get stream => const StreamStore();
+}
+
+class FutureStore {
+  const FutureStore();
+
+  Fetcher<String, List<String>> get futureFetcher => Fetcher.ofFuture(
+        (userId) async => [' _api.getUserTweets(userId)'],
+      );
+}
+
+class StreamStore {
+  const StreamStore();
+
+  Fetcher<String, List<String>> get streamFetcher =>
+      Fetcher.ofStream((userId) async* {
+        yield [' _api.getUserTweets(userId)'];
+      });
 }
