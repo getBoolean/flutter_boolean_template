bc3f9bb1b2d932aaf8a1438a3a816ae82e683529
getBoolean <danielwnewville@gmail.com>
2023-11-06 01:29:34 -0600

    Add topbar search button

--- lib/src/routing/ui/screens/home_screen.dart
+++ lib/src/routing/ui/screens/home_screen.dart
@@ -2,17 +2,20 @@ import 'package:auto_route/auto_route.dart';
 import 'package:constants/strings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_boolean_template/src/common_widgets/auto_adaptive_router_scaffold.dart';
+import 'package:flutter_boolean_template/src/features/settings/data/dto/settings.dart';
+import 'package:flutter_boolean_template/src/features/settings/data/repository/settings_provider.dart';
 import 'package:flutter_boolean_template/src/routing/router/app_router.dart';
 import 'package:flutter_boolean_template/utils/utils.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:log/log.dart';
 
 @RoutePage()
-class HomeScreen extends StatelessWidget implements AutoRouteWrapper {
+class HomeScreen extends ConsumerWidget implements AutoRouteWrapper {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
     const destinations = [
       RouterDestination(
@@ -32,6 +35,7 @@ class HomeScreen extends StatelessWidget implements AutoRouteWrapper {
       ),
     ];
 
+    final settings = ref.watch(appSettingsProvider);
     return SafeArea(
       child: AutoAdaptiveRouterScaffold(
         navigationTypeResolver: _resolveNavigationType,
@@ -40,6 +44,14 @@ class HomeScreen extends StatelessWidget implements AutoRouteWrapper {
           child: Text(
             kAppName,
             style: theme.textTheme.titleMedium?.merge(GoogleFonts.robotoMono()),
+          ),
+        ),
+        tabBarEnd: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: settings.isBannerShowing ? 60 : 4),
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
           ),
         ),
         drawerHeader: Row(
