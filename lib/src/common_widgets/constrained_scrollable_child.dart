26b1f80ea985f41487bc1744ca84ff0b9fd2e478
getBoolean <danielwnewville@gmail.com>
2023-11-06 21:47:57 -0600

    Add params for scrollable

--- lib/src/common_widgets/constrained_scrollable_child.dart
+++ lib/src/common_widgets/constrained_scrollable_child.dart
@@ -1,28 +1,93 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class ConstrainedScrollableChild extends StatelessWidget {
   const ConstrainedScrollableChild({
     super.key,
     required this.child,
-    this.direction = Axis.vertical,
+    this.scrollDirection = Axis.vertical,
+    this.controller,
+    this.primary,
+    this.physics,
+    this.dragStartBehavior = DragStartBehavior.start,
+    this.clipBehavior = Clip.hardEdge,
+    this.restorationId,
+    this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
   });
 
+  /// The widget that scrolls.
+  ///
+  /// {@macro flutter.widgets.ProxyWidget.child}
   final Widget child;
-  final Axis direction;
+
+  /// {@macro flutter.widgets.scroll_view.scrollDirection}
+  final Axis scrollDirection;
+
+  /// An object that can be used to control the position to which this scroll
+  /// view is scrolled.
+  ///
+  /// Must be null if [primary] is true.
+  ///
+  /// A [ScrollController] serves several purposes. It can be used to control
+  /// the initial scroll position (see [ScrollController.initialScrollOffset]).
+  /// It can be used to control whether the scroll view should automatically
+  /// save and restore its scroll position in the [PageStorage] (see
+  /// [ScrollController.keepScrollOffset]). It can be used to read the current
+  /// scroll position (see [ScrollController.offset]), or change it (see
+  /// [ScrollController.animateTo]).
+  final ScrollController? controller;
+
+  /// {@macro flutter.widgets.scroll_view.primary}
+  final bool? primary;
+
+  /// How the scroll view should respond to user input.
+  ///
+  /// For example, determines how the scroll view continues to animate after the
+  /// user stops dragging the scroll view.
+  ///
+  /// Defaults to matching platform conventions.
+  final ScrollPhysics? physics;
+
+  /// {@macro flutter.widgets.scrollable.dragStartBehavior}
+  final DragStartBehavior dragStartBehavior;
+
+  /// {@macro flutter.material.Material.clipBehavior}
+  ///
+  /// Defaults to [Clip.hardEdge].
+  final Clip clipBehavior;
+
+  /// {@macro flutter.widgets.scrollable.restorationId}
+  final String? restorationId;
+
+  /// {@macro flutter.widgets.scroll_view.keyboardDismissBehavior}
+  final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) => SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: switch (direction) {
-            Axis.vertical => BoxConstraints(minHeight: constraints.maxHeight),
-            Axis.horizontal => BoxConstraints(minWidth: constraints.maxWidth),
-          },
-          child: IntrinsicHeight(
-            child: child,
-          ),
-        ),
+        controller: controller,
+        primary: primary,
+        physics: physics,
+        dragStartBehavior: dragStartBehavior,
+        clipBehavior: clipBehavior,
+        restorationId: restorationId,
+        keyboardDismissBehavior: keyboardDismissBehavior,
+        scrollDirection: scrollDirection,
+        child: switch (scrollDirection) {
+          Axis.vertical => ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: IntrinsicHeight(
+                child: child,
+              ),
+            ),
+          Axis.horizontal => ConstrainedBox(
+              constraints: BoxConstraints(minWidth: constraints.maxWidth),
+              child: IntrinsicWidth(
+                child: child,
+              ),
+            ),
+        },
       ),
     );
   }
