aeb3996f5622a151d93f69f7895806c5182a1462
getBoolean <getboolean@gmail.com>
2023-12-11 08:07:42 +0000

    Deploying to gh-pages from @ getBoolean/flutter_boolean_template@e3212df9382310fedf09f47e6af439a511b4da89 🚀

--- main/flutter_service_worker.js
+++ main/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "d9c6f42c23888c34cbd03f47fb19ec16",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "4765f67a0ff4140ab1414be12f95a890",
-"/": "4765f67a0ff4140ab1414be12f95a890",
+"index.html": "0dbdcb4b631618785a48f4c5210f271e",
+"/": "0dbdcb4b631618785a48f4c5210f271e",
 "main.dart.js": "90531e94b659347fc736ccdb4da51d19",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/AssetManifest.json": "7888b9a1c6e6a19d3a1e1bde159f61e2",
