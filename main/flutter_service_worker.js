b1a933b5602ae01c61165a6a2df984ed3e729938
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-01-22 22:26:45 +0000

    Deploying to gh-pages from @ getBoolean/flutter_boolean_template@761b7fb4f411fee8a35a1ce9601aa9381209fc53 🚀

--- main/flutter_service_worker.js
+++ main/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "d9c6f42c23888c34cbd03f47fb19ec16",
-"index.html": "040593326cbc2cbcc0660ca3d8e35fed",
-"/": "040593326cbc2cbcc0660ca3d8e35fed",
+"index.html": "10f50bf4730de763fb3a4ddc46139e5e",
+"/": "10f50bf4730de763fb3a4ddc46139e5e",
 "assets/AssetManifest.bin": "87823796ee149ca291bc6aee8ae02034",
 "assets/fonts/MaterialIcons-Regular.otf": "4860fca79c261779afcf4f8d3c08cbf0",
 "assets/AssetManifest.bin.json": "3a4a2d536763a6e0930319555124a355",
