783313dc7ff1cd953f1b3c98188a558c5701e1c3
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-01-26 17:39:46 +0000

    Deploying to gh-pages from @ getBoolean/flutter_boolean_template@bf8612c7838db546119d817d053e9206264ec99a 🚀

--- chore/improve_deploy_web_comment_checks/flutter_service_worker.js
+++ chore/improve_deploy_web_comment_checks/flutter_service_worker.js
@@ -32,8 +32,8 @@ const RESOURCES = {"main.dart.js": "390bcb17731d60d8875463b2e50bf8ac",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a855aa6bffbd4c8c0b70d294f59bfd40",
-"/": "a855aa6bffbd4c8c0b70d294f59bfd40",
+"index.html": "ebce8aabf5729af375ae41226fe8a4dc",
+"/": "ebce8aabf5729af375ae41226fe8a4dc",
 "version.json": "c839b1dd8ab5bda2a3d1c4cc45e601fa",
 "manifest.json": "d9c6f42c23888c34cbd03f47fb19ec16"};
 // The application shell files that are downloaded before a service worker can
