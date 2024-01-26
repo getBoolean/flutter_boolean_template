04967d58f189f4e7db80941aa93f00b9662a5d1a
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-01-26 09:19:05 +0000

    Deploying to gh-pages from @ getBoolean/flutter_boolean_template@8d27bf72632f40a7c6e6b79a68170b0ffd026fee 🚀

--- feature/expandable_sidebar/flutter_service_worker.js
+++ feature/expandable_sidebar/flutter_service_worker.js
@@ -32,8 +32,8 @@ const RESOURCES = {"main.dart.js": "390bcb17731d60d8875463b2e50bf8ac",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "feb90cf98740c932db016c55b81617f8",
-"/": "feb90cf98740c932db016c55b81617f8",
+"index.html": "d66a76263493656e070bb1eaef4929eb",
+"/": "d66a76263493656e070bb1eaef4929eb",
 "version.json": "c839b1dd8ab5bda2a3d1c4cc45e601fa",
 "manifest.json": "d9c6f42c23888c34cbd03f47fb19ec16"};
 // The application shell files that are downloaded before a service worker can
