1634102f85f59538bb6207b0cdcdf6c736ea00ff
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-01-26 14:56:12 -0600

    revert temp changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_boolean_template/app.dart';
 import 'package:flutter_boolean_template/src/features/initialization/presentation/app_startup_widget.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,8 +18,10 @@ void main() async {
   }
 
   runApp(
-    AppStartupWidget(
-      onLoaded: (context) => const App(),
+    ProviderScope(
+      child: AppStartupWidget(
+        onLoaded: (context) => const App(),
+      ),
     ),
   );
 }
