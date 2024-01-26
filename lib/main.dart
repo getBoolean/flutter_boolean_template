05e0bf8e9949133e8f74c996a7faa0006bfa3cdb
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-01-26 14:51:26 -0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_boolean_template/app.dart';
 import 'package:flutter_boolean_template/src/features/initialization/presentation/app_startup_widget.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
