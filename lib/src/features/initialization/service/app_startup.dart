67447df583185419552d799a237542e54d2111d2
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-01-27 11:04:35 -0600

    Decrease min app size

--- lib/src/features/initialization/service/app_startup.dart
+++ lib/src/features/initialization/service/app_startup.dart
@@ -1,6 +1,7 @@
 import 'dart:io' as io;
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_boolean_template/src/features/initialization/service/info_service.dart';
 import 'package:flutter_boolean_template/src/features/settings/data/dto/navigation_type_override.dart';
 import 'package:flutter_boolean_template/src/features/settings/data/dto/settings.dart';
@@ -8,6 +9,7 @@ import 'package:flutter_boolean_template/src/features/settings/data/repository/s
 import 'package:flutter_boolean_template/utils/utils.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:window_manager/window_manager.dart';
 
 part 'app_startup.g.dart';
 
@@ -36,7 +38,7 @@ Future<void> _initHive() async {
 Future<void> _initWindow() async {
   if (!kIsWeb &&
       (io.Platform.isWindows || io.Platform.isLinux || io.Platform.isMacOS)) {
-    // await windowManager.ensureInitialized();
-    // await windowManager.setMinimumSize(const Size(600, 300));
+    await windowManager.ensureInitialized();
+    await windowManager.setMinimumSize(const Size(350, 300));
   }
 }
