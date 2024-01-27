cbc7a9af5fb289caf24216f41d9dbeb31721432e
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-01-27 08:32:47 +0000

    Deploying to gh-pages from @ getBoolean/flutter_boolean_template@b473eee7389af281f81dd95d67ec1288375cb688 🚀

--- feature/149-SidebarAppbar/flutter_service_worker.js
+++ feature/149-SidebarAppbar/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "907a0354c720a3766a286cd06fec6440",
+const RESOURCES = {"main.dart.js": "5b93c163d74b387f5107a3f4d245b570",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "assets/fonts/MaterialIcons-Regular.otf": "0b620565ec174491a4dc76fa8453ea33",
@@ -32,8 +32,8 @@ const RESOURCES = {"main.dart.js": "907a0354c720a3766a286cd06fec6440",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "989e47742713cf2ad0941d847f826ca8",
-"/": "989e47742713cf2ad0941d847f826ca8",
+"index.html": "0a0bb73a0bad8d0b810f38509e374c33",
+"/": "0a0bb73a0bad8d0b810f38509e374c33",
 "version.json": "c839b1dd8ab5bda2a3d1c4cc45e601fa",
 "manifest.json": "d9c6f42c23888c34cbd03f47fb19ec16"};
 // The application shell files that are downloaded before a service worker can
