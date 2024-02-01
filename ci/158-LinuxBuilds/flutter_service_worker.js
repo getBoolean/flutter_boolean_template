5a768fce0d87504865a62dfecbde66b5c686f895
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-02-01 03:58:58 +0000

    Deploying to gh-pages from @ getBoolean/flutter_boolean_template@bf9fca1b9f687d76b4df77bb26e109530735d50e 🚀

--- ci/158-LinuxBuilds/flutter_service_worker.js
+++ ci/158-LinuxBuilds/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "c839b1dd8ab5bda2a3d1c4cc45e601fa",
-"index.html": "fe27d240ae7e8a11d16800368afb2794",
-"/": "fe27d240ae7e8a11d16800368afb2794",
+"index.html": "ea5ebfefac2c34f194ef5ef95bd93c70",
+"/": "ea5ebfefac2c34f194ef5ef95bd93c70",
 "main.dart.js": "1a9911d8360881167cd2323114685d40",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
