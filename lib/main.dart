00a0dba750623210a78b3525f199eafe2a1dabba
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-03-03 13:23:53 -0600

    fix error

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() async {
   // * https://docs.flutter.dev/testing/errors
   registerErrorHandlers();
   AppFlavor.initConfig();
-  const env = EnvFlavor.instance;
+  final env = EnvFlavor.instance;
   if (env.usePathUrlStrategy) {
     usePathUrlStrategy();
   }
