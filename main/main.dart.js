843bc2e4848ec4234956c008cd2d2f2c7a42ca4c
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-03-09 20:41:08 +0000

    Deploying to gh-pages from @ getBoolean/flutter_boolean_template@4962c3217a1c13f4a4efa4f720f5f422b62056f4 🚀

--- main/main.dart.js
+++ main/main.dart.js
@@ -33892,7 +33892,7 @@ A.aO5.prototype={
 $1(a){var s=A.iL().b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/04817c99c9fd4956f27505204f7e344335810aed/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/2e4ba9c6fb499ccd4e81420543783cc7267ae406/":s)+a},
 $S:58}
 A.aOm.prototype={
 $1(a){this.a.remove()
