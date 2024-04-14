528e1b0fd8de4e7595c04d320c1781bff92a39ff
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-04-13 22:04:44 -0500

    Update method docs

--- packages/pub/pagination_dart/lib/src/keep_alive_duration.dart
+++ packages/pub/pagination_dart/lib/src/keep_alive_duration.dart
@@ -5,7 +5,7 @@ import 'package:riverpod/riverpod.dart';
 extension KeepAliveDurationExtension<T> on AutoDisposeRef<T> {
   /// Keeps a [Ref] alive for a given [duration].
   ///
-  /// Any inflight requests will need to be cancelled separately.
+  /// Any inflight requests will need to be cancelled separately during [onDispose].
   void keepAliveDuration([Duration duration = const Duration(seconds: 30)]) {
     // When a provider is no-longer used, keep it in the cache for some time
     final link = keepAlive();
